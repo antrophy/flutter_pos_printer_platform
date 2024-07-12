191056c474cad0ec9871a41e5cfe0c4147c07857
Ivan Andric <ivan@aduro.hr>
2024-07-12 09:56:43 +0200

    Change man activity

--- example/android/app/src/main/kotlin/com/sersoluciones/flutter_pos_printer_platform_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/sersoluciones/flutter_pos_printer_platform_example/MainActivity.kt
@@ -3,4 +3,8 @@ package com.sersoluciones.flutter_pos_printer_platform_example
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+     override fun onCreate(savedInstanceState: Bundle?) {
+       super.onCreate(savedInstanceState)
+       GeneratedPluginRegistrant.registerWith(this)
+   }
 }
